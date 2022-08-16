b14fab22bd3121a9be1ff96ed2dc95b60877c4f6
AriyanDC <ariyan.vafaie@gmail.com>
2022-08-16 16:42:36 +0300

    Complete ray

--- Ray.js
+++ Ray.js
@@ -1,14 +1,21 @@
 class Ray {
-    constructor(x, y, dirx, diry) {
+    constructor(x, y, angle) {
         this.pos = createVector(x, y);
-        this.dir = createVector(dirx, diry)
+        this.dir = p5.Vector.fromAngle(angle);
+    }
+
+
+
+    lookAt = (px, py) => {
+        this.dir.set(px - this.pos.x, py - this.pos.y);
+        this.dir.normalize();
     }
 
     display = () => {
         push()
         translate(this.pos.x, this.pos.y);
         stroke(0, 255, 0);
-        line(0, 0, this.dir.x * 50, this.dir.y * 50)
+        line(0, 0, this.dir.x * 30, this.dir.y * 30)
         pop()
     }
 
@@ -17,17 +24,20 @@ class Ray {
         const x1 = obs.a.x;
         const x2 = obs.b.x;
         const x3 = this.pos.x;
-        const x4 = this.pos.x + this.dir.x;
+        const x4 = this.pos.x + this.dir.x * width;
         const y1 = obs.a.y;
         const y2 = obs.b.y;
         const y3 = this.pos.y;
-        const y4 = this.pos.y + this.dir.y;
+        const y4 = this.pos.y + this.dir.y * height;
 
 
         const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-        const t = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) / den
-        const u = (x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2) / den
+        const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+        const u = ((x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2)) / den;
 
+        if (t > 0 && t < 1 && u > 0) {
+            return createVector(x1 + t * (x2 - x1), y1 + t * (y2 - y1))
+        }
 
 
     }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,29 @@
 let r;
 let o;
+// let sl;
+// let sly;
 
 function setup() {
     createCanvas(400, 400);
-    r = new Ray(30, 200, 1, 0);
-    o = new Obstacle(300, 100, 350, 300)
+    r = new Ray(30, 200, .5, .1);
+    o = new Obstacle(300, 100, 300, 300)
+        // sl = createSlider(.1, 1, 0, .1);
+        // sly = createSlider(.1, 1, 0, .1);
 }
 
 function draw() {
+
+    // r.dir.x = sl.value();
+    // r.dir.y = sly.value();
+
     background(130);
     r.display();
     o.display();
-
-    let pt = r.cast(wall);
+    r.lookAt(mouseX, mouseY);
+    let pt = r.cast(o);
     if (pt) {
         fill(255);
-        ellipse(pt.x, pt.y);
+        ellipse(pt.x, pt.y, 10);
     }
+
 }
